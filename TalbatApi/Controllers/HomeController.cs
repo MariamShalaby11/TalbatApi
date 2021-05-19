32e4dd2ecbeaa44d3c6a854e4a00b81070d1790d
HanyaHesham <HanyaHesham@users.noreply.github.com>
2021-05-19 16:01:58 +0200

    uncomment

--- TalbatApi/Controllers/HomeController.cs
+++ TalbatApi/Controllers/HomeController.cs
@@ -11,7 +11,6 @@ namespace TalbatApi.Controllers
         public ActionResult Index()
         {
             ViewBag.Title = "Home Page";
-            //shdhudhudhudhdu
 
             return View();
         }
